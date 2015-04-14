951817c4bfe8cd157e87df3b3e3743ba40bc841a
Sushma Reddy <sushmareddy@Sushmas-MacBook-Air.local>
2015-04-14 11:39:05 -0500

    Initial Commit

---
+++ selfie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  selfie
+//
+//  Created by Sushma Reddy on 4/14/15.
+//  Copyright (c) 2015 Sushma Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
